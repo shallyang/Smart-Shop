48129b635d393aacb9c55bc25c0d465f52ddd8dd
张不骄傲 <1140107656@qq.com>
2017-03-03 19:22:24 +0800

    模板foot部分删除

--- resources/views/admins/moban.blade.php
+++ resources/views/admins/moban.blade.php
@@ -290,15 +290,7 @@
 
             @show
             <!-- Inner Container End -->
-            
-            <!-- Footer -->
-            <div id="mws-footer">
-            	Copyright Your Website 2012. All Rights Reserved.
-            </div>
-            
-        </div>
-        <!-- Main Container End -->
-        
+        </div>    
     </div>
 
     <!-- JavaScript Plugins -->
