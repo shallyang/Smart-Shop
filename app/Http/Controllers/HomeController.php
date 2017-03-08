bf9fde01897b1890389d6c75f035c92df1a2e120
244876563rm <244876563@qq.com>
2017-03-08 21:29:16 +0800

    前台用户登录修改

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,9 +34,23 @@ class HomeController extends Controller
         return $obj;
     }
 
+    // 登录页
     public function getLogin()
     {
     	return view('/homes/login');
     }
+
+    // 判断用户是否存在
+    public function postLogins(Request $request)
+    {
+    	echo '这是前台首页';
+    }
+
+    //注册页
+    public function getRegister(Request $request)
+    {
+    	return view('/homes/login');
+    }
+
    
 }
--- resources/views/homes/muban.blade.php
+++ resources/views/homes/muban.blade.php
@@ -132,6 +132,7 @@ $(document).ready(function() {
 					<h1><a href="index.html"><span>S</span>mart</a></h1>
 					<h6>你的超市. 你的地盘.</h6> 
 				</div>	
+				@section('search')
 				<div class="header-search">
 					<form action="#" method="post">
 						<input type="search" name="Search" placeholder="请输入需要搜索的商品" required="">
@@ -140,6 +141,7 @@ $(document).ready(function() {
 						</button>
 					</form>
 				</div>
+				@show
 				<div class="header-cart"> 
 					<div class="my-account">
 						<a href="contact.html"><i class="fa fa-map-marker" aria-hidden="true"></i> 联系我们</a>						
