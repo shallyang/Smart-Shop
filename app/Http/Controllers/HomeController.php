56fe28aa3b31b11e89ad54c5da6cf7f6619eb353
shallyang <yxc930708@gmail.com>
2017-03-08 20:52:06 +0800

    模板分类遍历

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,16 +6,32 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use DB;
 class HomeController extends Controller
 {
     /**
      * 首页
      */
-    public function getIndex()
-    {
-        return view('homes/index');
-    }
 
-    
+    public function getIndex(Request $request)
+    {
+    	$res = DB::table('goods_type')->
+       select(DB::raw("*,concat(pathid,',',id) as paths"))->
+            orderBy('paths')->get();
+        return view('homes/index',['res'=>$res]);
+    }
+    public static function getCate($pid)
+    {
+        //先确认顶级信息
+        $cates = DB::table('goods_type')->where('pid','=',$pid)->get();
+        $obj = [];
+        foreach ($cates as $k => $v) {
+        //使用递归来实现深度查找
+           $v->nu = self::getCate($v->id);
+           
+           $obj[] = $v;
+        }
+        return $obj;
+    }
+   
 }
