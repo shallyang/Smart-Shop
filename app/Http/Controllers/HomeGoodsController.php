1426bee342fc0867dc441802114b7a61aec1c2ae
张不骄傲 <1140107656@qq.com>
2017-03-09 19:32:21 +0800

    商品列表显示

--- app/Http/Controllers/HomeGoodsController.php
+++ app/Http/Controllers/HomeGoodsController.php
@@ -18,21 +18,32 @@ class HomeGoodsController extends Controller
     {
         //查询包含所传值得所有的子类
         $res = DB::table('goods_type')->where('pid','=',$id)->get();
-        echo'<pre>';
+        //判断子类深度
         $obj = [];
         if($res == null){
             $obj[] = DB::table('goods_table')->where('typeid','=',$id)->get();
-       }
-        var_dump($obj);
-        // foreach($res as $k=>$v){
-        //     //获取子类的主id来获得子商品
-        //     $tid =$v->id;
-        //     echo $tid;
-        //     $obj[] = DB::table('goods_table')->where('typeid','=',$tid)->get();
-        // }
+         }
+        foreach($res as $k=>$v){
+            //获取子类的主id来获得子商品
+            $tid =$v->id;
+            $obj[] = DB::table('goods_table')->where('typeid','=',$tid)->get();
+        }
+               //获取子类商品的图片信息
+        $gobj =[];
         // echo'<pre>';
-        // var_dump($obj);
-        return view('homes/product');
+        // var_dump($obj);die;
+        $row = [];
+        foreach($obj as $gk => $gv){
+            foreach($gv as $lgk => $lgv){
+            $row[] = $lgv;
+            $gid = $lgv->goodsid;
+            $gobj[] = DB::table('goods_pic_table')->where('goodsid','=',$gid)->first();
+            }
+           
+        }
+        // echo'<pre>';
+        // dd($gobj);
+        return view('homes/product',['row'=>$row,'gobj'=>$gobj]);
     }
 
    
--- resources/views/homes/product.blade.php
+++ resources/views/homes/product.blade.php
@@ -2,8 +2,8 @@
 @section('title','商品列表')
 @section('content')
 	<ol class="breadcrumb breadcrumb1">
-		<li><a href="index.html">Home</a></li>
-		<li class="active">Products</li>
+		<li><a href="index.html">主页</a></li>
+		<li class="active"></li>
 	</ol> 
 	<div class="clearfix"> </div>
 	<!-- //breadcrumbs -->
@@ -32,24 +32,32 @@
 		<div class="clearfix"> </div>
 	</div>
 	<div class="products-row">
+
+			@foreach($row as $k => $v)
+
+
 		<div class="col-md-3 product-grids"> 
+			
 			<div class="agile-products">
-				<div class="new-tag"><h6>20%<br>Off</h6></div>
-				<a href="single.html"><img src="images/e1.png" class="img-responsive" alt="img"></a>
+			
+				<a href="single.html"><img src="" class="img-responsive" alt="img" width='100%'></a>
+			
 				<div class="agile-product-text">              
-					<h5><a href="single.html">Audio speaker</a></h5> 
-					<h6><del>$200</del> $100</h6> 
+					<h5><a href="single.html">{{$v->goodsname}}</a></h5> 
+					<h6>¥{{$v->goodsprice}}</h6> 
 					<form action="#" method="post">
 						<input type="hidden" name="cmd" value="_cart">
 						<input type="hidden" name="add" value="1"> 
 						<input type="hidden" name="w3ls_item" value="Audio speaker"> 
-						<input type="hidden" name="amount" value="100.00"> 
+						<input type="hidden" name="amount" value="100.00">
 						<button type="submit" class="w3ls-cart pw3ls-cart"><i class="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>
 					</form> 
 				</div>
-			</div> 
+
+			</div> 	
+			
 		</div>
+		@endforeach
 		<div class="clearfix"> </div>
 	</div>
-
 @endsection
